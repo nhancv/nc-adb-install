1a241c976917e307c52cd1d7241c29f15a7bf595
Nhan Cao <nhancv92@gmail.com>
2017-05-30 14:58:07 +0700

    update bash on windows

--- adb_win/android-util.bat
+++ adb_win/android-util.bat
@@ -6,67 +6,58 @@ ECHO 2. Force install apk
 ECHO --------------------
 SET /p commandNumber="Which command you want? "
 IF %commandNumber%==1 GOTO :INSTALL_URL
-IF NOT %commandNumber%==1 GOTO :FORCE_INSTALL
+IF %commandNumber%==2 GOTO :FORCE_INSTALL
+
+GOTO :END
 
 :INSTALL_URL
-
-
+SET /p APK_URL="Enter the url to apk: "
+adb shell am start -a android.intent.action.VIEW -d '%APK_URL%'
 GOTO :END
 
 :FORCE_INSTALL
 SET /p PATH_NAME="Enter the path to apk: "
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`aapt dump badging C:\adb_win\avb-staging_7.apk`) DO (
+FOR /F "tokens=* USEBACKQ" %%F IN (`aapt dump badging %PATH_NAME%`) DO (
 SET APK_INFO=%%F
-CALL :FIND_POS
-CALL SET APK_PACKAGE=%%APK_INFO:~15,%pos%%%
 GOTO :FORCE_INSTALL_BREAK
 )
 :FORCE_INSTALL_BREAK
+
+CALL :FIND_POS
+CALL SET APK_PACKAGE=%%APK_INFO:~15,%pos%%%
 ECHO %APK_INFO%
 
 ECHO ---------begin---------
 
-CALL :FORCE_STOP
-CALL :CLEAR_APP
-CALL :UNINSTALL
-CALL :INSTALL
-CALL :START_APP
+ECHO force-stop: %APK_PACKAGE%
+adb shell am force-stop %APK_PACKAGE%
+
+ECHO clear: %APK_PACKAGE%
+adb shell pm clear %APK_PACKAGE%
+
+ECHO uninstall: %APK_PACKAGE%
+adb uninstall %APK_PACKAGE%
+
+ECHO install: %PATH_NAME%
+adb install -r %PATH_NAME%
+
+ECHO start: %APK_PACKAGE%
+adb shell monkey -p %APK_PACKAGE% -c android.intent.category.LAUNCHER 1
 
 ECHO ---------end---------
 GOTO :END
-
-:FORCE_STOP
-ECHO force-stop: %APK_PACKAGE%
-adb shell am force-stop %APK_PACKAGE%
-EXIT /B 0
-:CLEAR_APP
-ECHO clear: %APK_PACKAGE%
-adb shell pm clear %APK_PACKAGE%
-EXIT /B 0
-:UNINSTALL
-ECHO uninstall: %APK_PACKAGE%
-adb uninstall %APK_PACKAGE%
-EXIT /B 0
-:INSTALL
-ECHO install: %APK_PACKAGE%
-adb install -r %APK_PACKAGE%
-EXIT /B 0
-:START_APP
-ECHO start: %APK_PACKAGE%
-adb shell monkey -p %APK_PACKAGE% -c android.intent.category.LAUNCHER 1
-EXIT /B 0
 
 :FIND_POS
 Set "str1=%APK_INFO%"
 Set "sstr=\' versionCode="
 SET stemp=%str1%&SET pos=0
-:FIND_POS_loop
+:FIND_POS_LOOP
 SET /a pos+=1
 echo %stemp%|FINDSTR /b /c:"%sstr%" >NUL
 IF ERRORLEVEL 1 (
 SET stemp=%stemp:~1%
-IF DEFINED stemp GOTO FIND_POS_loop
+IF DEFINED stemp GOTO FIND_POS_LOOP
 SET pos=0
 )
 SET /a pos-=16
